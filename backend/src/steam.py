8a101d3285d16d57a81d7d2c282bf423bb7639ee
Guillaume SCHNEIDER <guillaumeschneider@siggraph.org>
2024-12-07 10:55:43 +0100

    fix: async

--- backend/src/steam.py
+++ backend/src/steam.py
@@ -1,7 +1,12 @@
-import requests
+import httpx
 
 
-def get_game_by_id(id: int):
+async def get_game_by_id(id: int, client):
     url = f"https://store.steampowered.com/api/appdetails/?appids={id}"
-    response = requests.get(url)
-    return response.json()
+    try:
+        response = await client.get(url)
+        response.raise_for_status()
+        return response.json()
+    except Exception as e:
+        print(f"Error fetching Steam data for {id}: {e}")
+        return None
